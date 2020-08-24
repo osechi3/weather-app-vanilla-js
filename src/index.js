205565b954b39c1f0ac0b4978cec6661f2de2948
osechi3 <holypoetwrk@gmail.com>
2020-08-24 09:52:16 +0300

    Moving elements in the App class

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ export class App {
   static weatherData = new WeatherData()
   static searchBar = new SearchBar()
   static mainContainer = document.createElement('div')
+  static rootElement = document.querySelector('#app')
 
   static init () {
     this.render()
-    this.weatherData.getWeatherData()
   }
 
   static render () {
-    const rootElement = document.querySelector('#app')
-    rootElement.append(this.mainContainer)
+    this.rootElement.append(this.mainContainer)
     this.mainContainer.append(this.searchBar.render())
   }
 
