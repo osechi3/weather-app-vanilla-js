731a6bc5ba47cd4f7d264c275ac6a080d957232c
osechi3 <holypoetwrk@gmail.com>
2020-08-24 10:54:37 +0300

    Setting the parent element of SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,18 @@ import { Component } from './Component'
 import { App } from '../index'
 
 export class SearchBar extends Component {
+  constructor (parentElement, shouldRender) {
+    super(parentElement, shouldRender)
+    this._eslintUselessConstructor = 0
+  }
+
   render () {
-    const containerSearch = this.createElement('div', 'container-search')
+    const containerSearch = this.createBaseElement('div', 'container-search')
     containerSearch.innerHTML = `
       <input type="text" placeholder="Enter city"></input>
       <button>Search</button>
     `
     const button = containerSearch.querySelector('button')
     button.addEventListener('click', App.sendWeatherRequest.bind(App))
-    return containerSearch
   }
 }
