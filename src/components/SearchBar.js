eb8219cd9bff3c9f1633babf5f84451b593018ce
osechi3 <holypoetwrk@gmail.com>
2020-08-24 09:49:04 +0300

    Adding an event listener that sends a request

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import { Component } from './Component'
+import { App } from '../index'
 
 export class SearchBar extends Component {
   render () {
@@ -7,5 +8,7 @@ export class SearchBar extends Component {
       <input type="text" placeholder="Enter city"></input>
       <button>Search</button>
     `
+    const button = containerSearch.querySelector('button')
+    button.addEventListener('click', App.sendWeatherRequest.bind(App))
   }
 }
